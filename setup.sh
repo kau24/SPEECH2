ae03e35b70c2217b3a53693f9a6ffd57f583b31b
kau24 <168022214+kau24@users.noreply.github.com>
2024-07-16 12:27:33 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-echo 'No system dependencies to install'
+# Download and install ffmpeg
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz | tar xJ
+export PATH=$PATH:$PWD/ffmpeg-*-static
